3fd8ee5b225361d4d490ed2354fcff4d71d6e0c2
Daniar <daniar.ah55@gmail.com>
2019-04-21 00:56:58 +0600

    Implemented localstorage

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
+import ls from 'local-storage';
+
+console.log(ls.get('accessToken'));
 
 export default axios.create({
     baseURL: 'http://localhost:3000/',
-    headers: {'Content-Type': 'application/json'}
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': ls.get('accessToken') || ''
+    }
 });
